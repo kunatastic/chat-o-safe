f37d48330b0924c8905443de02fa87b7b08894c4
kunatastic <kjha6666@gmail.com>
2021-04-14 17:57:19 +0530

    Coord issues resolved

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,26 +2,47 @@ import React, { useState, useEffect } from "react";
 import { socket } from "./socketHooks/Socket";
 import Location from "./Components/Location";
 
+interface locationType {
+  longitude: number;
+  latitude: number;
+  accuracy: number;
+  altitude: number;
+  altitudeAccuracy: number;
+  timeStamp: number;
+}
+
 function App() {
   const [data, setData] = useState(1);
-  const [cord, setCord] = useState({ empty: true });
+  // const [cord, setCord] = useState<locationType[]>([]);
+  const [cord, setCord] = useState({
+    longitude: 0,
+    latitude: 0,
+    accuracy: 0,
+    altitude: 0,
+    altitudeAccuracy: 0,
+    timeStamp: 0,
+  });
 
-  const dummy = (cord: any) => {
-    setCord(cord);
+  const storeLocation = (loactionData: locationType) => {
+    console.log(loactionData);
+    setCord(loactionData);
   };
   useEffect(() => {
     socket.emit("msg", "HELLO THERE");
+    // console.log(cord);
   }, []);
 
   useEffect(() => {
     socket.emit("msg", "Already connected");
   }, [data]);
+
   return (
     <>
       Hello World
       <button onClick={() => setData(data + 1)}>btn</button>
-      <Location />
-      {cord}
+      <button onClick={() => console.log(cord)}>loc</button>
+      <Location storeLocation={storeLocation} />
+      {/* {user} */}
     </>
   );
 }
--- client/src/Components/Location.tsx
+++ client/src/Components/Location.tsx
@@ -1,4 +1,15 @@
-function Location() {
+interface locationType {
+  longitude: number;
+  latitude: number;
+  accuracy: number;
+  altitude: number;
+  altitudeAccuracy: number;
+  timeStamp: number;
+}
+
+type PropsFunction = (coord: locationType) => void;
+
+function Location({ storeLocation }: { storeLocation: PropsFunction }) {
   function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
@@ -13,7 +24,17 @@ function Location() {
     //   position.coords.latitude +
     //   "<br>Longitude: " +
     //   position.coords.longitude;
-    console.log(position);
+    // console.log(position);
+    var loc: locationType = {
+      longitude: position.coords.longitude,
+      latitude: position.coords.latitude,
+      accuracy: position.coords.accuracy,
+      altitude: position.coords.altitude,
+      altitudeAccuracy: position.coords.altitudeAccuracy,
+      timeStamp: position.timestamp,
+    };
+    // console.log(loc);
+    storeLocation(loc);
   }
   return (
     <div>
